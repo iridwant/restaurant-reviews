dabf109c3dfb8c366c4a20ffbd5a9358500708f8
ekaps11 <ekaputras369@gmail.com>
2020-11-19 17:30:19 +0700

    adding db relation

---
+++ controllers/ControllerCustomer.js
@@ -0,0 +1,5 @@
+class ControllerCustomer {
+    // 
+}
+
+module.exports = ControllerCustomer
--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Restaurant.hasMany(models.RestaurantReview)
     }
   };
   Restaurant.init({
