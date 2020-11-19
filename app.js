ec83d4e2f9441cbe6b205ab0126f443fcf2da1e6
iridwant <iftikarridwan@gmail.com>
2020-11-19 17:12:06 +0700

    setup views

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('./customers/register')
 })
 
 app.listen(port, () => {
